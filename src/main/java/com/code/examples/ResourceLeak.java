707102d7c481a6f98721fd7edbfc8f055692fe78
13501275443 <33673174+13501275443@users.noreply.github.com>
2020-06-07 20:48:34 -0700

    Update ResourceLeak.java

--- src/main/java/com/code/examples/ResourceLeak.java
+++ src/main/java/com/code/examples/ResourceLeak.java
@@ -3,6 +3,9 @@ package com.code.examples;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 class ResourceLeak {
   public String readFromFile(final String filename) {
@@ -25,4 +28,15 @@ class ResourceLeak {
 
     return ret;
   }
+
+  public void openConnection(final String connectionString) {
+    try {
+      final Connection conn = DriverManager.getConnection(connectionString);
+      conn.prepareCall("select * from test_table");
+      conn.close();
+    } catch (final SQLException e) {
+      System.out.println(e.toString());
+    }
+  }
+
 }
