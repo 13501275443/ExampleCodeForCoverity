0a18a6ceeaa7373c74ef7859b2e193be6b28eed0
13501275443 <33673174+13501275443@users.noreply.github.com>
2020-06-08 07:48:41 -0700

    Create Employee.java

---
+++ src/main/java/com/code/examples/Employee.java
@@ -0,0 +1,25 @@
+package com.code.examples;
+
+class Employee {
+  private int id;
+  private String name;
+
+  public Employee() {
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(final int empId) {
+    id = empId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String empName) {
+    name = empName;
+  }
+}
