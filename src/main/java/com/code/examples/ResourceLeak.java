fbf16500ef7df51e1206b48e31a454d9750cf793
13501275443 <33673174+13501275443@users.noreply.github.com>
2020-06-09 13:16:30 -0700

    Update ResourceLeak.java

--- src/main/java/com/code/examples/ResourceLeak.java
+++ src/main/java/com/code/examples/ResourceLeak.java
@@ -1,18 +1,18 @@
 package com.code.examples;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 class ResourceLeak {
   public String readFromFile(final String filename) {
     String ret = "";
     try {
-      final FileReader fileReader = new FileReader(filename);
-      final BufferedReader bufferedReader = new BufferedReader(fileReader);
+      final InputStream fileReader = new FileInputStream(filename);
+      final Reader reader = new InputStreamReader(fileReader, "UTF-8");
+      final BufferedReader bufferedReader = new BufferedReader(reader);
       //try {
       String line;
       while ((line = bufferedReader.readLine()) != null) {
@@ -32,7 +32,10 @@ class ResourceLeak {
   public void openConnection(final String connectionString) {
     try {
       final Connection conn = DriverManager.getConnection(connectionString);
-      conn.prepareCall("select * from test_table");
+      final Statement statement = conn.createStatement();
+      if (statement.execute("update employee set name='test' where id=5")) {
+        System.out.println("Update employee table successfully");
+      }
       conn.close();
     } catch (final SQLException e) {
       System.out.println(e.toString());
