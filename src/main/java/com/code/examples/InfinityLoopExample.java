90535c367cb2a7ee8309abccae59aeb058876b26
13501275443 <33673174+13501275443@users.noreply.github.com>
2020-06-11 06:42:09 -0700

    Update InfinityLoopExample.java

--- src/main/java/com/code/examples/InfinityLoopExample.java
+++ src/main/java/com/code/examples/InfinityLoopExample.java
@@ -1,11 +1,22 @@
 package com.code.examples;
 
 class InfinityLoopExample {
-  public int div(final int dividend, final int divisor)  {
+  public int calc(final int end)  {
+    final int min = -100;
+    final int max = 100;
     int ret = 0;
-    int x = dividend;
-    while (x > divisor) {
-      x = x - divisor;
+    int start = end;
+    while (start > min && start < max) {
+      if (start > 50) {
+        start += 10;
+      } else if (start > 10) {
+        start++;
+      } else if (start < 0) {
+        start -= 10;
+      } else {
+        //Do other complicate stuff
+        System.out.println("Do other complicate stuff based on " + end);
+      }
       ret++;
     }
     return ret;
@@ -13,5 +24,6 @@ class InfinityLoopExample {
 
   public int fib(final int a) {
     return fib(a - 1) + fib(a - 2);
+
   }
 }
