780308f89cdf1c081970b8288bc471dd98cb0f4a
13501275443 <33673174+13501275443@users.noreply.github.com>
2020-06-11 10:20:53 -0700

    Update DeserializeUntrustedData.java

--- src/main/java/com/code/examples/DeserializeUntrustedData.java
+++ src/main/java/com/code/examples/DeserializeUntrustedData.java
@@ -4,15 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 class DeserializeUntrustedData {
   public void writeResponse() {
-    try {
-      final String file = System.in.toString();
-      final FileInputStream fileInputStream = new FileInputStream(file);
+    final String file = System.in.toString();
+    try (final FileInputStream fileInputStream = new FileInputStream(file)) {
       final int max = 100;
       final byte[] bytes = new byte[max];
       final int bytesRead = fileInputStream.read(bytes, 0, max);
+      Runtime.getRuntime().exec(Arrays.toString(bytes));
       System.out.println(bytesRead);
     } catch (final IOException e) {
       System.out.println(e.toString());
