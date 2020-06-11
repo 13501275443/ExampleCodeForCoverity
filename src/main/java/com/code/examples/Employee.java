1135561cd68a23e38b8168904f0fd323d5ec7954
13501275443 <33673174+13501275443@users.noreply.github.com>
2020-06-11 05:55:06 -0700

    Update Employee.java

--- src/main/java/com/code/examples/Employee.java
+++ src/main/java/com/code/examples/Employee.java
@@ -1,6 +1,10 @@
 package com.code.examples;
 
-class Employee {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+class Employee implements Serializable {
   private int id;
   private String name;
 
@@ -21,5 +25,10 @@ class Employee {
 
   public void setName(final String empName) {
     name = empName;
+  }
+
+  private  void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+    in.defaultReadObject();
+    this.name = this.name + " virus attack!!!";
   }
 }
